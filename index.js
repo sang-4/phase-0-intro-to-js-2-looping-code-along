3e0e63937b9a017b2d450f9f7147e84f4ff76a03
Allan <kipropsallan@gmail.com>
2022-06-07 15:48:09 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(names)
+{
+    let messages = [];
+    for (let id=0; id<names.length; id++){
+        messages.push(`Thank you, ${names[id]}, for the wonderful surprise gift!`);
+    }
+    return messages;
+}
+function countDown(n)
+{
+    while (n>0){
+        console.log(n)
+        n--;
+    }
+    console.log(n)
+}
